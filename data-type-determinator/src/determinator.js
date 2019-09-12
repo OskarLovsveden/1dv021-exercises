d5dbc4f8bcf70c579e7919092406c4589b3a2e98
OskarLovsveden <ol222hf@student.lnu.se>
2019-09-12 14:13:55 +0200

    Adding final functionality and comments

--- data-type-determinator/src/determinator.js
+++ data-type-determinator/src/determinator.js
@@ -18,25 +18,36 @@
 function tellType (data) {
 
   let result = 'You sent me a'
+  // Gets the type of data as a 'string'.
   let type = typeof data
 
+  // if type = 'undefined'...
   if (type === 'undefined') {
-    result += 'n undefined value'
+    result += 'n undefined value.'
 
+    // else if type = 'string', 'number', 'boolean' or 'function'...
   } else if (type === 'string' ||
       type === 'number' ||
       type === 'boolean' ||
       type === 'function'
   ) {
 
-    result += ` ${type}`
-  } else if (Array.isArray(data)) {
-    result += 'n array'
-  
-  } else if (type === ) {
-    result += 'n null value'
+    result += ` ${type}.`
+    // else if type = 'object' check if...
+  } else if (type === 'object') {
+    // 'object' is an array...
+    if (Array.isArray(data)) {
+      result += 'n array.'
+      // a value of 'null'...
+    } else if (data === null) {
+      result += ' null value.'
+      // or just a "normal" 'object'.
+    } else {
+      result += 'n object.'
+    }
   }
 
+  // Return the result of what we found.
   return result
 }
 
